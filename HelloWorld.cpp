dbc308531c4b5dd97ae73ad279c21d10945cf1b9
Ryan West <rockoverlord23@gmail.com>
2021-05-24 20:39:10 -0400

    First Commit for assignment 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,21 @@
+// Simple C++ program to display "Hello World" 
+//Author is Ryan <rockoverlord23@gmail.com>
+  
+// Header file for input output functions 
+
+//Adding comments for fun and conflict
+//Version of master branch
+
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello world"; 
+      
+    return 0; 
+} 
